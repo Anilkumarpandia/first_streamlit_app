0284ff7bd6dbadab347899c9b2cf6e941ee221d7
Anilkumarpandia <142223500+Anilkumarpandia@users.noreply.github.com>
2023-08-18 14:10:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,4 @@
 import streamlit
-streamlit.title('my parents new healthey dinner')
-streamlit.header('breakfast menu')
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale,Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
-
-import streamlit
 
 streamlit.title("my mom's new healthy dinner")
 streamlit.header('breakfast Fevorites')
