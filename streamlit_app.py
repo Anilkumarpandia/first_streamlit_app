4d7264e11b16097d2ad25020136f8167dd85b104
Anilkumarpandia <142223500+Anilkumarpandia@users.noreply.github.com>
2023-08-18 14:40:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit
 streamlit.title("my mom's new healthy dinner")
 streamlit.header('Breakfast Fevorites')
-#streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-#streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
-#streamlit.text('🐔Hard-Boiled Free-Range Egg')
-#streamlit.text('🥑🍞Avocado Toast')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
 
-#import pandas
-#my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#my_fruit_list = my_fruit_list.set_index('Fruit')
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-#streamlit.dataframe(my_fruit_list)
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
